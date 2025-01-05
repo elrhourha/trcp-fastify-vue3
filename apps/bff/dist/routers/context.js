a5bc696d11720c4c5048b79dffb19c65910a5b89
kelrhourha <karim.elrhourha@gmail.com>
2025-01-05 10:43:31 +0100

    work with tsc-alias

--- apps/bff/dist/routers/context.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createContext = createContext;
-function createContext({ req, res }) {
-    const user = { name: req.headers.username ?? 'anonymous' };
-    return { req, res, user };
-}
